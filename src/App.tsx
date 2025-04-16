3b0ba61f5f3a3309fc461db200809d98979b27f2
OyePriscilla <oyebadepriscilla22@gmail.com>
2025-04-16 18:24:44 +0100

    Handle error temporary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { NavBar } from "../types";
 
 import Home from "./pages/Home";
@@ -21,14 +21,14 @@ const NavBars: NavBar[] = [
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Nav />
         <Routes>
           {NavBars.map((item) => (
             <Route key={item.path} path={item.path} element={item.element} />
           ))}
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
